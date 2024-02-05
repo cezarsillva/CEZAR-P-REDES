fcbebbc2776980ed98da7c4a1af34a44dfdc35aa
cezarsillva <158162683+cezarsillva@users.noreply.github.com>
2024-02-05 23:13:13 +0000

    Exerccios feito

--- aula2.py
+++ aula2.py
@@ -1,21 +1,21 @@
-print('funciona!!!!')
-nota1 = 7
-print(nota1)
-print(type(nota1))
+#print('funciona!!!!')
+#nota1 = 7
+#print(nota1)
+#print(type(nota1))
 
-print(f'nota1 tem valor {nota1} e é tipo {type(nota1)} ')
-nota2 = 7.3
-print(f'nota1 tem valor {nota2} e é tipo {type(nota2)} ')
-aluno = 'Maria'
-print(f'aluno tem valor {aluno} e é tipo {type(aluno)} ')
-media = (nota1 + nota2) / 2
-print(f'{aluno}\n{nota1}\n{nota2}\n{media} ')
+#print(f'nota1 tem valor {nota1} e é tipo {type(nota1)} ')
+#nota2 = 7.3
+#rint(f'nota1 tem valor {nota2} e é tipo {type(nota2)} ')
+#aluno = 'Maria'
+#print(f'aluno tem valor {aluno} e é tipo {type(aluno)} ')
+#media = (nota1 + nota2) / 2
+#print(f'{aluno}\n{nota1}\n{nota2}\n{media} ')
 
-soma = nota1 + nota2
-divisao = nota1 / nota2
-multiplicacao = nota1 * nota2
-subtracao = nota1 - nota2
-resto_divisao = nota1 % nota2
+#soma = nota1 + nota2
+#divisao = nota1 / nota2
+#multiplicacao = nota1 * nota2
+#subtracao = nota1 - nota2
+#resto_divisao = nota1 % nota2
 
 
 #exemplo de interação do usuario.
@@ -34,10 +34,23 @@ resto_divisao = nota1 % nota2
 #print(f'{nota1} % {nota2} = {resto}')
 
 
-valor = float(input ('Digite o valor do produto:'))
-desconto = float(input ('Digite o desconto do produto:')) 
+#valor = float(input ('Digite o valor do produto:'))
+#desconto = float(input ('Digite o desconto do produto:')) 
 
-valortotal = valor * (1 - desconto / 100)
+#valortotal = valor * (1 - desconto / 100)
 
-print(f'O valor do produto é {valortotal}.')
+#print(f'O valor do produto é {valortotal}.')
 
+#-------------------------------------------------------------------------
+##########################################################################
+
+
+#Exercicios para proxima aula:
+
+
+preço = float(input ('Digite o valor do produto: '))
+porcent = int(input ('Digite a Pocentagem para desconto: '))
+novopreco = preço - (preço * porcent / 100 )
+
+
+print(f'O valor do produto com desconto é : {novopreco}.')
