500878eb1718b5ed5835564cb5422e45c5b5f08a
cezarsillva <158162683+cezarsillva@users.noreply.github.com>
2024-04-03 23:26:27 +0000

    certo

---
+++ lista4.py
@@ -0,0 +1,10 @@
+
+
+
+
+while True:
+    time = int(input('Digite o múmero correspondente ao time ou zero para sair do programa: Fluminense = 1 Botafogo = 2 Vasco = 3 Flamengo = 4 Outros = 5 Sair = 0:  '))
+    if time == 0:
+        break
+    endereco = int(input('Digite o endereço do pesquisado sendo Rio de janeiro = 1 Niterói = 2 Outros = 3: '))
+    salario = float(input('Digite o salario do pesquisado: '))
--- teste.py
+++ teste.py
@@ -18,8 +18,8 @@
 #• o número de torcedores por clube; --------------------------------------------------- ok
 #• a média salarial dos torcedores do Botafogo;----------------------------------------- ok
 #• o número de pessoas moradoras do Rio de Janeiro, torcedores de outros
-#clubes;
-#• o número de pessoas de Niterói torcedoras do Fluminense
+#clubes;----------------ok
+#• o número de pessoas de Niterói torcedoras do Fluminense----------------ok
 #Obs.: O programa encerra quando se digita 0 para o time.
 
 
@@ -33,16 +33,23 @@ quantidaderiodejaneiro = 0
 quantidadeniteroi = 0
 quantidadeoutrosenderecos = 0
 
+totalbotafogo = 0
 
-totalbotafogo = 0.0
 
-for x in range(3):
+
+for x in range(5):
+	print('\n')
 	print(f'-------------------------------------------------------------')
 	print(f'Torcedor:{x+1}')
 	print('\n')
-	time = int(input('Digite o nome do time sendo Fluminense = 1 Botafogo = 2 Vasco = 3 Flamengo = 4 Outros = 5:  '))
+	
+	time = int(input('Digite o múmero correspondente ao time ou zero para sair do programa: Fluminense = 1 Botafogo = 2 Vasco = 3 Flamengo = 4 Outros = 5 Sair = 0:  '))
 	endereco = int(input('Digite o endereço do pesquisado sendo Rio de janeiro = 1 Niterói = 2 Outros = 3: '))
 	salario = float(input('Digite o salario do pesquisado: '))
+	
+
+	print('\n')
+
 	if time == 1:
 		quantidadefluminense += 1
 		print(f' Torcedor do Fluminense.')
@@ -73,18 +80,23 @@ for x in range(3):
 		print(f' Torcedor mora em outras Cidades.')
 	else:
 		print(f'O opção inválida.......')
-		
 
-		
+
+
+
+
+
+
+
 mediabotafogo = totalbotafogo / quantidadebotafogo
 
 
 print('\n')
-print(f'----------------------------------------------------------------')
+print(f'###################################################################')
 
 print(f'A média salarial dos torcedores do Botafogo é: {mediabotafogo}')
 
-print(f'----------------------------------------------------------------')
+print(f'###################################################################')
 
 print('\n')
 
@@ -98,11 +110,14 @@ print(f'A quantidade de torcedores do Flamengo  é: {quantidadeflamengo} ')
 
 print(f'A quantidade de torcedores dos outros clubes  é: {quantidadeoutros} ')
 
+print('\n')
+print('\n')
 
+print(f'O número de pessoas moradoras do Rio de Janeiro é {quantidaderiodejaneiro} e a quantidade de torcedores de outros times é {quantidadeoutros}')
+	
+print(f'O número de pessoas de Niterói é {quantidadeniteroi} e torcedoras do Fluminense é {quantidadefluminense}')
 
+print('\n')
+print('\n')
 
-
-
-
-
-print(f'O número de pessoas moradoras do Rio de Janeiro, torcedores de outros')
+print(f'-------------------------------------------------------------')
